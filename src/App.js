4dce99a07aed5c62c26b30f667995dda52d49a86
Ian Williamson <ian@4of.uk>
2022-11-02 15:32:20 +0000

    add basket to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import Search from "./components/Search";
+import Basket from "./components/Basket/basket";
+
 
 function App() {
   return (
-    <div>
-      <Search />
-    </div>
+    <>
+      <div>
+        <Search />
+      </div>
+      <div>
+        <Basket />
+      </div>
+    </>
   );
 }
 
