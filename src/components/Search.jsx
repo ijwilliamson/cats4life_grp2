5ae5234effc95edf76268cb6c0395633b5d57071
Mayranne <mayranne-@hotmail.com>
2022-11-02 23:51:18 +0000

    Add new global font color in index.css and change all the css from the cards and change the api to just display jpg or png images

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -34,18 +34,44 @@ const Search = () => {
         return (
           <div className="cat-card-container">
             <div className="cat-card">
-              <p className="cat-description-name">{catDetails[i].name}</p>
               <img className="cat-images" key={i} src={cat.url} alt="cat" />
-              <p className="cat-description-breed">{catDetails[i].breed}</p>
+              <div className="cat-description-header">
+                <p className="cat-description-name">{catDetails[i].name}</p>
+                <p className="cat-description-breed">{catDetails[i].breed}</p>
+              </div>
               <p className="cat-description-text">
                 Hello, my name is <strong>{catDetails[i].name}</strong>! I am
                 looking for my forever home and I promise to never wake you up
                 at 4am asking for food.
               </p>
-              <p className="cat-description-fee">
-                Adoption fee: £{catDetails[i].price}
-              </p>
-              <button className="cat-description-btn">Adopt me</button>
+              <hr className="line" />
+              <div className="cat-description-mid">
+                <p className="cat-description-mid-title">
+                  Reasons that I'm perfect for you:
+                </p>
+                <div className="cat-reasons">
+                  <p>
+                    <i class="fa-solid fa-cat"></i> New buddie
+                  </p>
+                  <p>
+                    <i class="fa-sharp fa-solid fa-bowl-food"></i> Easy to care
+                  </p>
+                  <p>
+                    <i class="fa-solid fa-heart heart"></i> Lovely
+                  </p>
+                </div>
+              </div>
+              <hr className="line" />
+              <div className="cat-description-footer">
+                <button className="cat-description-btn">Adopt me</button>
+                <div className="cat-fee">
+                  <p className="cat-description-fee">Adoption fee</p>
+                  <p className="cat-description-fee">£{catDetails[i].price}</p>
+                </div>
+                <button className="cat-description-like">
+                  <i class="fa-solid fa-heart"></i>
+                </button>
+              </div>
             </div>
           </div>
         );
