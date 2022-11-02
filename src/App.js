6d43cc551cca824707e3114b9adf26c6b2c7992e
Ian Williamson <ian@4of.uk>
2022-11-02 18:10:59 +0000

    add cat object to state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import "./App.css";
 import Search from "./components/Search";
-import Basket from "./components/Basket/Basket";
+import Basket from "./components/Basket/basket";
 import { useState } from "react";
 
+
 function App() {
+
   // State to store the cats in the basket
   const [basketItems, setBasketItems] = useState();
+  
 
-  const addToBasket = (cat) => {
+  const addToBasket = (cat) =>{
     //function which is called by the search component to add the
     // cat to the basket.
     // The cat is passed as an object
-  };
+
+    let tempBasket = [...basketItems];
+    tempBasket.push(cat)
+
+    setBasketItems(tempBasket)
+
+  }
+
 
   return (
+
     <div>
       <div className="nav1">
+       
         <p>All Products</p>
         <input type="text"></input>
         <ul>
@@ -23,8 +35,10 @@ function App() {
           <a href="#">Cart</a>
         </ul>
       </div>
-      <Search />
+      <Search callback={addToBasket} />
     </div>
+     
+
   );
 }
 
