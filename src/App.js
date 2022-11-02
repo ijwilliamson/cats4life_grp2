fe0d0c71c0b915cedcef95422965c09550e0511d
Yan5577 <zuoyan775@gmai.com>
2022-11-02 15:57:16 +0000

    add header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import Search from "./components/Search";
 function App() {
   return (
     <div>
-      <Search />
+      <div className="nav1">
+        {/* <Search /> */}
+        <p>All Products</p>
+        <input type="text"></input>
+        <ul>
+          <a href="#">Account</a>
+          <a href="#">Cart</a>
+        </ul>
+      </div>
     </div>
   );
 }
