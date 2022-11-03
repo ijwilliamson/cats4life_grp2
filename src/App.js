b13b354f5680f5327fb3106728f5d6aadd2c8eef
Yan5577 <zuoyan775@gmai.com>
2022-11-03 10:58:40 +0000

    add header files

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import Search from "./components/Search";
 import Basket from "./components/Basket/Basket";
 import { useState } from "react";
 import Footer from "./components/Footer";
-import CatsIcon from "./cat.png";
-import CartIcon from "./cart.png";
-import SearchIcon from "./searchlogo.png";
-
+import Header from "./components/Header/Header";
 function App() {
   // State to store the cats in the basket
 
@@ -38,21 +35,10 @@ function App() {
         basketState={basketItems}
       />
       <div>
-      <div className="nav1">
-        <img className="cat" src={CatsIcon} alt="cat"></img>
-        <span>Cats4Lyf</span>
-        <input type="text"></input>
-        <img className="searchlogo" src={SearchIcon} alt="sl"></img>
-
-        <img
-          className="cartlogo"
-          onClick={toggleBasket}
-          src={CartIcon}
-          alt="cart"
-        ></img>
-      </div>
       
-
+        <Header
+          toggle={toggleBasket}
+        />
       <Search callback={addToBasket} />
 
         <Footer />
