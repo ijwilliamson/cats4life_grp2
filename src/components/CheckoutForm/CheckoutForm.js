d40dcf88995b0008077ecbb3c6961a0f90a1a090
Ian Williamson <ian@4of.uk>
2022-11-04 13:47:33 +0000

    complete checkout form

--- src/components/CheckoutForm/CheckoutForm.js
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -1,11 +1,20 @@
 import './CheckoutForm.css'
-
+import { useNavigate } from 'react-router-dom';
 const CheckoutForm = () => {
+    const nav = useNavigate();
+
+    const handleFormSubmit =(event) =>{
+        event.preventDefault();
+       
+        nav("/");
     
+    }
+
+
     return (
         <div className="cF">
         <h1>Check out</h1>
-        <form>
+        <form onSubmit={handleFormSubmit}>
             <hor-group>
                 <section>
                     <label htmlFor="fName">First name </label>
@@ -41,8 +50,31 @@ const CheckoutForm = () => {
                     <input id="postcode"  type="text"/>
                 </section>
             </ver-group>
-            
-
+            <ver-group>
+            <section>
+                    <label htmlFor="tel">Telephone </label>
+                    <input id="tel"  type="text"/>
+                </section>
+                <section>
+                    <label htmlFor="mobile">Mobile</label>
+                    <input id="mobile"  type="text"/>
+                </section>
+                <section>
+                    <label htmlFor="email">Email</label>
+                    <input id="email"  type="text"/>
+                </section>
+                <section>
+                    <label htmlFor="cEmail">Confirm Email</label>
+                    <input id="cEmail"  type="text"/>
+                </section>
+            </ver-group>
+            <div className="footer">
+          
+         
+            <button className="cat-description-btn" type='submit' >Back to store</button>
+            <button className="cat-description-btn" type='button'>Pay now</button>
+           
+        </div>
         </form>
      </div>
     )
